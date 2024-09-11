651df68b30ff7cb9ef66dc8da06335a64a9f25d7
CarrieCanDo <carrie.a.wright@outlook.com>
2024-09-11 09:48:38 -0400

    Initial commit for Part 2

---
+++ src/main/java/com/example/SpringBootSecuringApisPartTwo/SpringBootSecuringApisPartTwoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootsecuringapisparttwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecuringApisPartTwoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSecuringApisPartTwoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/SpringBootSecuringApisPartTwo/SpringBootSecuringApisPartTwoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootsecuringapisparttwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecuringApisPartTwoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
